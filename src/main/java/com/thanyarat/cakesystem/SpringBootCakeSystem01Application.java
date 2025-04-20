c1645e05ac5a8a32696a10c58d211cf5f2a8efd0
thany <thanyarat0984@gmail.com>
2025-04-20 22:23:07 +0700

    first commit

---
+++ src/main/java/com/thanyarat/cakesystem/SpringBootCakeSystem01Application.java
@@ -0,0 +1,13 @@
+package com.thanyarat.cakesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCakeSystem01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCakeSystem01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/thanyarat/cakesystem/SpringBootCakeSystem01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thanyarat.cakesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCakeSystem01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
