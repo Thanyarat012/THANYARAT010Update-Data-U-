fcc2654eacb8d58b7f38e5e99c31bb2b71e98436
thany <thanyarat0984@gmail.com>
2025-04-22 14:26:31 +0700

    first commit

--- src/main/java/com/thanyarat/cakesystem/controller/StudentController.java
+++ src/main/java/com/thanyarat/cakesystem/controller/StudentController.java
@@ -3,8 +3,12 @@ package com.thanyarat.cakesystem.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.thanyarat.cakesystem.service.StudentService;
@@ -27,4 +31,14 @@ public class StudentController {
 	public List<Students> getAllStudents() {
 		return StudemtService.findAll();
 	}
+	@PutMapping("/{id}")
+	public ResponseEntity<Students> updateStudent(@PathVariable Integer id, @RequestBody Students updatedStudent) {
+	    Students student = StudentService.updateStudent(id, updatedStudent);
+	    if (student != null) {
+	        return ResponseEntity.ok(student);
+	    } else {
+	        return ResponseEntity.notFound().build();
+	    }
+	}
+
 }
