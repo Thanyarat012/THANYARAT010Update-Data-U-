d1f329678d0da832fa0dc527342516db2e98e805
thany <thanyarat0984@gmail.com>
2025-04-22 13:57:10 +0700

    first commit

--- src/main/java/com/thanyarat/cakesystem/repository/StudentRepository.java
+++ src/main/java/com/thanyarat/cakesystem/repository/StudentRepository.java
@@ -1,11 +1,9 @@
 package com.thanyarat.cakesystem.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import ch.qos.logback.core.joran.event.SaxEvent;
 
-@Repository
 public interface StudentRepository  extends JpaRepository<SaxEvent,Integer>{
 
 }
